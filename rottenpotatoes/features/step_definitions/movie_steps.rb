36524ebd0719eef3e6c2294055808335c329bbab
nguyenhuearmy <huearmynguyen@gmail.com>
2016-04-28 13:05:21 +0000

    complete test cucumber

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,24 +4,28 @@ Given /^the following movies exist:$/ do |movies_table|
   end
 end
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+Then /^the director of "([^"]*)" should be "([^"]*)"$/ do |arg1, arg2|
+   assert page.body =~ /#{arg1}.+Director.+#{arg2}/m
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  assert page.body =~ /#{e1}.+#{e2}/m
+end
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  if uncheck == "un"
+    rating_list.split(', ').each {|x| step %{I uncheck "ratings_#{x}"}}
+  else
+    rating_list.split(', ').each {|x| step %{I check "ratings_#{x}"}}
+  end
 end
 
-Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+Then /I should not see any of the movies/ do
+  rows = page.all('#movies tr').size - 1
+  assert rows == 0
+end
+
+Then /I should see all of the movies/ do
+  rows = page.all('#movies tr').size - 1
+  assert rows == Movie.count()
 end
